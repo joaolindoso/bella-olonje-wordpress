720b4686de10b6cf78728fe15cbf57af21955171
João Lindoso <joao.lindoso@seati.ma.gov.br>
2021-09-16 15:36:56 -0300

    Adicionando chamada para Post Type Banner no functions

--- functions.php
+++ functions.php
@@ -137,220 +137,38 @@ add_action("save_post", "save_custom_meta_box", 10, 3);
 */
 
 
+// Register Custom Post Type Banners
+function banners_post_type() {
 
+	$labels = array(
+		'name'                  => _x( 'Banners', 'Post Type General Name', 'banners' ),
+		'singular_name'         => _x( 'Banner', 'Post Type Singular Name', 'banners' ),
+		'menu_name'             => __( 'Banners', 'banners' ),
+		'name_admin_bar'        => __( 'Banners', 'banners' ),
+	);
+	$args = array(
+		'label'                 => __( 'Banners', 'banners' ),
+		'description'           => __( 'Nossos Banners', 'banners' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'comments' ),
+		'taxonomies'              => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-format-aside',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'banners', $args );
 
-/*
-* Criando Post Type para os Banners
-*/
-
-add_action('init', 'post_type_banner');
-
-function post_type_banner() {
-    register_post_type(
-        'banner',
-        array(
-            'labels' => array(
-                 'name' => __( 'Banners' ),
-                 'singular_name' => __( 'Banner' ),
-                 'add_new' => __('Adicionar Banner', 'Novo Banner'),
-			     'add_new_item' => __('Novo Banner'),
-			     'edit_item' => __('Editar Banner'),
-			     'new_item' => __('Novo Banner'),
-			     'view_item' => __('Ver Banner'),
-			     'search_items' => __('Procurar Banners'),
-                 'all_items' => __('Todos os Banners'),                
-			     'not_found' =>  __('Nenhum Banner encontrado'),
-			     'not_found_in_trash' => __('Nenhum Banner encontrado na lixeira'),
-			     'parent_item_colon' => '',
-			     'menu_name' => 'Banners'
-                ),
-            'description' => __('Registro de Banners'),
-            'public' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'menu_position' => 5,
-            'show_in_nav_menus' => true,
-            'show_in_admin_bar' => true,
-            'register_meta_box_cb' => 'banners_meta_boxes',
-            'publicly_queryable' => true,
-            'query_var' => true,
-            'rewrite' => array( 'slug' => 'banner' ),
-            'capability_type' => 'post',
-            'has_archive' => true,
-            'Hierarchical' => false,
-            'can_export' => true,
-            'exclude_from_search' => false,
-            'supports' => array(
-                'title',
-				'content',
-				'excerpt',
-                'author',
-                'thumbnail')
-                /* Nota: foi retirado suporte ao editor */
-        )
-    );
-    register_taxonomy_for_object_type('categoria', 'banner');
-    flush_rewrite_rules();
 }
-
-/*
-* Criando a taxonomia para o tipo Banners
-*/
-
-register_taxonomy(
-"categoria", 
-"banner", 
-      array(            
-      	"label" => "Categorias", 
-        "singular_label" => "Categoria", 
-        "rewrite" => true,
-        "hierarchical" => true
-	  )
-);
-
-/*****
-Criando os campos personalizados
-*****/
-
-add_action( 'admin_init', 'banners_meta_boxes' );
-
-function banners_meta_boxes() {
-    add_meta_box( 'chapeu', __('Chapéu'), 'chapeu_meta_box', 'banner', 'normal', 'high' );
-}
-
-/*****
-Criando cada apresentação HTML dos campos personalizados no form do post type Banners
-*****/
-
-function identificacao_linha_meta_box() {
-    global $post;
-    $nrLinha = get_post_meta($post->ID, '_nr_linha', true);
-    $empresa = get_post_meta($post->ID, '_empresa', true);
-    if ( function_exists('wp_nonce_field') ) wp_nonce_field('identificacao_linha_nonce', '_identificacao_linha_nonce');
-        ?>
-            <label for="_nr_linha">Nr. da Linha</label>
-            <input type="text" size="10" name="_nr_linha" value="<?php echo wp_specialchars(stripslashes($nrLinha), 1); ?>" />
-            <label for="_empresa">Empresa</label>
-            <input type="text" size="50" name="_empresa" value="<?php echo wp_specialchars(stripslashes($empresa), 1); ?>" />
-        <?php
-}
-
-/*****
- Salvando os dados do Post Type Banners
-*****/
-
-add_action( 'save_post', 'save_banner_meta_data' );
-
-function save_banner_meta_data( $post_id ) {
-    global $post;
-    
-    // ignore autosaves
-    /*if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return $post_id;
-    
-    // check nonces
-    check_admin_referer('identificacao_linha_nonce', '_identificacao_linha_nonce');
-    check_admin_referer('atendimento_nonce', '_atendimento_nonce');
-    check_admin_referer('integrado_nonce', '_integrado_nonce');
-    check_admin_referer('horarios_nonce', '_horarios_nonce');
-
-    // check capabilites
-    if ( 'banner' == $_POST['post_type'] && !current_user_can( 'edit_post', $post_id ) )
-    return $post_id;*/
-
-    // salvando campos, um por um
-    
-    update_post_meta($post_id, '_nr_linha', $_POST['_nr_linha']);
-    update_post_meta($post_id, '_empresa', $_POST['_empresa']);
-    update_post_meta($post_id, '_atendimento', $_POST['_atendimento']);
-
-    update_post_meta($post_id, '_integrado', $_POST['_integrado']);
-    
-    update_post_meta($post_id, '_diasuteisprimeira', $_POST['_diasuteisprimeira']);
-    update_post_meta($post_id, '_diasuteisultima', $_POST['_diasuteisultima']);
-    
-    update_post_meta($post_id, '_sabadoprimeira', $_POST['_sabadoprimeira']);
-    update_post_meta($post_id, '_sabadoultima', $_POST['_sabadoultima']);
-    
-    update_post_meta($post_id, '_domingoferiadoprimeira', $_POST['_domingoferiadoprimeira']);
-    update_post_meta($post_id, '_domingoferiadoultima', $_POST['_domingoferiadoultima']);
-    
-    update_post_meta($post_id, '_mapa', $_POST['_mapa']);
-}
-
-/* Customização das colunas de edição na lista de Banners */
-
-add_action("manage_posts_custom_column",  "banner_custom_columns");
-add_filter("manage_edit-banner_columns", "banner_edit_columns");
- 
-function banner_edit_columns($columns){
-  $columns = array(
-    "cb" => "<input type='checkbox' />",
-    "title" => "Banner",
-    "_atendimento" => "Atendimento",
-    "_nr_linha" => "Linha",
-    "categoria" => "Categoria",
-    "terminalintegracao" => "Terminal de Integração",
-    "empresa" => "Empresa",
-  );
- 
-  return $columns;
-}
-
-function banner_custom_columns($column) {
-  global $post;
- 
-  switch ($column) {
-    case "_atendimento":
-      $custom = get_post_custom();
-      echo $custom["_atendimento"][0];
-      break;
-    case "_nr_linha":
-      $custom = get_post_custom();
-      echo $custom["_nr_linha"][0];
-      break;
-    case "categoria":
-      echo get_the_term_list($post->ID, 'categoria');
-      break;
-    case "terminalintegracao":
-      echo get_the_term_list($post->ID, 'terminalintegracao','','<br>');
-      break;
-    case "empresa":
-      echo get_the_term_list($post->ID, 'empresa','' ,'<br>');
-      break;
-  }
-}
-
-/*****
- Função para permitir ordenação pelo campo
-*****/
-
-add_filter( 'manage_edit-banner_sortable_columns', 'banner_sortable_columns' );
-
-function banner_sortable_columns( $columns ) {
-    $columns['_nr_linha'] = '_nr_linha';
-	$columns['categoria'] = 'categoria';
-    $columns['terminalintegracao'] = 'terminalintegracao';
-    $columns['empresa'] = 'empresa';
-	return $columns;
-}
+add_action( 'init', 'banners_post_type', 0 );
 
 
-
-/*****
- Função para personalizar a pesquisa no custom post type Banners
-*****/
-
-function searchfilter($query) {
-    if ($query->is_search && !is_admin() ) {
-        if(isset($_GET['post_type'])) {
-            $type = $_GET['post_type'];
-                if($type == 'banner') {
-                    $query->set('post_type',array('banner'));
-                }
-        }       
-    }
-return $query;
-}
-add_filter('pre_get_posts','searchfilter');
-
-/* fim da criação do custom post type */
