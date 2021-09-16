4cae0c49828e56d38036b96ceb7a248548d8424a
João Lindoso <joao.lindoso@seati.ma.gov.br>
2021-09-16 18:44:29 -0300

    Corrigindo loops dos banners post types

--- index.php
+++ index.php
@@ -1,23 +1,39 @@
 <?php get_header(); ?>
 
+<!-- Banner post type loop -->
+<?php 
+    $acao_query2 = new WP_Query( array('post_type'=>'banners', 'category_name'=>'banner-topo', 'posts_per_page'=>1, 'order'=>'DESC'));
 
-<!-- Banner post type -->
-<div id="banner-topo" class="p-5 text-center bg-light text-white bg-banner-top d-flex justify-content-center align-items-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12" id="text-banner-top">
-          <h3 id="hat-banner-top">Food app</h3>
-          <h1 class="mb-3">Why stay hungry when you can order form Bella Onojie</h1>
-          <h3 id="second-title-banner-top">Download the bella onoje's food app now on</h3>
-          <div class="btn-banner-top d-flex flex-md-row flex-column justify-content-center align-items-center mt-5 pt-3">
-            <a href="#" class="general-btn-rounded btn-red btn-size-top">Playstore</a>
-            <a href="#" class="general-btn-rounded btn-wit-top btn-size-top">App store</a>
+    while ( $acao_query2->have_posts() ) : $acao_query2->the_post();
+?>
+
+        <div id="banner-topo" class="p-5 text-center bg-light text-white d-flex justify-content-center align-items-center" style="min-height: 619px; background-image: url('<?php echo get_the_post_thumbnail_url();?>')">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12" id="text-banner-top">
+                <h3 id="hat-banner-top">
+                    <?php 
+                        $tags = get_tags(); 
+                        foreach ( $tags as $tag ) { 
+                          echo $tag->name; 
+                        } 
+                    ?>
+                </h3>
+                <h1 class="mb-3">
+                    <?php the_title(); ?>
+                </h1>
+                <h3 class="second-title-banner-top">
+                   <?php the_excerpt(); ?>
+                </h3>
+                <div class="btn-banner-top d-flex flex-md-row flex-column justify-content-center align-items-center mt-5 pt-3">
+                  <?php the_content(); ?>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-
+        
+<?php endwhile; wp_reset_query(); ?> 
 
 
 <section id="food-for-everyone" class="w-100">
@@ -54,50 +70,60 @@
         while ( $acao_query1->have_posts() ) : $acao_query1->the_post();
     ?>
 
-            
-                    <li class="row mt-2 mb-5 p-5">
-                        <div class="col-md-5 m-0 text-left gridInverseMobile1">
-                            <?php the_post_thumbnail('full', array( 'class' => 'img-fuid shadow-img'));?>
-                        </div>
-                        <div class="col-md-7 d-flex flex-column justify-content-center gridInverseMobile2">
-                        
-                            <?php /* Incluindo o campo Chapéu */
-                                $campo_chapeu = get_post_meta($post->ID, '_chapeu', true);              
-                                echo '<h3 class="hat-how-to-work text-orange mb-4">'.$campo_chapeu.' </h3>'; 
-                            ?>
-                            
-                            <h2 class="title-how-to-work mb-4">
-                                <?php the_title(); ?>
-                            </h2>
-
-                            <h4 class="secondary-title-how-to-work text-gray">
-                                <?php the_excerpt(); ?>
-                            </h4>
-                        </div>
-                    </li>
+            <li class="row mt-2 mb-5 p-5">
+                <div class="col-md-5 m-0 text-left gridInverseMobile1">
+                    <?php the_post_thumbnail('full', array( 'class' => 'img-fuid shadow-img'));?>
+                </div>
+                <div class="col-md-7 d-flex flex-column justify-content-center gridInverseMobile2">
                 
+                    <?php /* Incluindo o campo Chapéu */
+                        $campo_chapeu = get_post_meta($post->ID, '_chapeu', true);              
+                        echo '<h3 class="hat-how-to-work text-orange mb-4">'.$campo_chapeu.' </h3>'; 
+                    ?>
+                    
+                    <h2 class="title-how-to-work mb-4">
+                        <?php the_title(); ?>
+                    </h2>
+
+                    <h4 class="secondary-title-how-to-work text-gray">
+                        <?php the_excerpt(); ?>
+                    </h4>
+                </div>
+            </li>
 
     <?php endwhile; wp_reset_query(); ?> 
 
 </ul>
 
-<!-- Banner post type -->
-
+<!-- Banner post type loop -->
 <!-- versão normal -->
-<div id="version-normal-footer" class="p-2 text-center text-white bg-banner-footer d-flex justify-content-center align-items-center">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12" id="text-banner-footer">
-        <h2 class="title-banner-footer text-white mb-3">Download the app now.</h2>
-        <h4 class="second-title-banner-footer">Available on your favorite store. Start your premium experience now</h4>
-        <div class="btn-banner-footer mt-5 pt-3">
-          <a href="#" class="general-btn-squad-rounded btn-red btn-size-footer">Playstore</a>
-          <a href="#" class="general-btn-squad-rounded btn-wit btn-size-footer">App store</a>
+
+<?php 
+    $acao_query2 = new WP_Query( array('post_type'=>'banners', 'category_name'=>'banner-rodape', 'posts_per_page'=>1, 'order'=>'DESC'));
+
+    while ( $acao_query2->have_posts() ) : $acao_query2->the_post();
+?>
+
+        <div id="version-normal-footer" class="p-2 text-center text-white d-flex justify-content-center align-items-center" style="min-height: 619px; background-image: url('<?php echo get_the_post_thumbnail_url();?>')">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12" id="text-banner-footer">
+                <h2 class="title-banner-footer text-white mb-3">
+                    <?php the_title(); ?>
+                </h2>
+                <h4 class="second-title-banner-footer">
+                   <?php the_excerpt(); ?>
+                </h4>
+                <div class="btn-banner-footer mt-5 pt-3">
+                  <?php the_content(); ?>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
+        
+<?php endwhile; wp_reset_query(); ?> 
+
 
 
 
